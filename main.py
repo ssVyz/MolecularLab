7246b0c088df9642d6a59805378101a145af8475
dnz <dom.nrz@gmail.com>
2026-09-02 17:16:21 +0200

    putting objects into rooms

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ def main() -> None:
     ######### Placeholder tests ##########
     print("provisional test suite")
 
-    print("Test 1: Make an object and check its contents")
-    testobj1 = Lab_object()
-    testobj1.quick_setup("test1", 5, 2, 2)
-    testobj1.show_info()
+    print("Test 1: rooms and objects")
+    daRoom = Room()
+    daRoom.quick_setup(50)
+    obj1 = Lab_object("Megablaster 5000")
+    obj1.quick_setup("test1", 5, 2, 2)
+    obj1.show_info()
+    ret = daRoom.place_lab_object(obj1)
+    print(ret)
+    daRoom.show_info()
 
     print("End of content")
 
--- objects.py
+++ objects.py
@@ -1,30 +1,27 @@
 
 
-class Lab:
-
-    def __init__(self) -> None:
-        self.rooms: list[Room] = []
-        self.workers: list[Worker] = []
 
 
-class Room:
 
-    def __init__(self) -> None:
-        self.area: int = 0
-        self.objects: list[Lab_object] = []
-
+### LAB OBJECT CLASS ###
+########################
 
 class Lab_object:
 
-    def __init__(self) -> None:
+    def __init__(self, name: str) -> None:
         self.obj_type_code: str = ""
+        self.obj_name: str = name
         self.size: int = 0
         self.weight: int = 0
         self.max_weight: int = 0
         self.contains: Lab_object | None = None
 
+        self.status: str = "standby"
+        self.max_processes: int = 0
+
+
     def show_info(self) -> None:
-        print(f"Obj type: {self.obj_type_code}, contains: {self.contains}")
+        print(f"Obj type: {self.obj_type_code}, obj name: {self.obj_name}, contains: {self.contains}")
 
     def quick_setup(self, code: str, size: int, weight: int, max_weight: int) -> None:
         self.obj_type_code = code
@@ -32,6 +29,47 @@ class Lab_object:
         self.weight = weight
         self.max_weight = max_weight
 
+
+
+
+### ROOM CLASS ###
+##################
+
+class Room:
+
+    def __init__(self) -> None:
+        self.area: int = 0
+        self.objects: list[Lab_object] = []
+
+    def quick_setup(self, size: int) -> None:
+        if size >= 0 and isinstance(size, int):
+            self.area = size
+
+    def place_lab_object(self, o: Lab_object) -> str:
+        if isinstance(o, Lab_object):
+            self.objects.append(o)
+            return "Object successfully added"
+        else:
+            return "error trying to add the object"
+
+    def show_info(self) -> None:
+        print(f"Area: {self.area}, objects: {self.objects}")
+
+
+
+### LAB CLASS ###
+#################
+
+class Lab:
+
+    def __init__(self) -> None:
+        self.rooms: list[Room] = []
+        self.workers: list[Worker] = []
+
+
+
+### Worker class, to be moved elsewhere ###
+##########################################
 
 class Worker:
 
