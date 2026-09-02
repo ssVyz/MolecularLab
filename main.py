1c5a88e5104dc77acff2c90fa399cdc85c7629c3
dnz <dom.nrz@gmail.com>
2026-09-02 10:07:13 +0200

    initial setup

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+
+def main():
+
+
+    ######### Placeholder tests ##########
+    print("provisional test suite")
+
+
+if __name__ == "__main__":
+    main()
---
+++ objects.py
@@ -0,0 +1,35 @@
+
+
+class Lab:
+
+    def __init__(self) -> None:
+        self.rooms: list[Room] = []
+        self.workers: list[Worker] = []
+
+
+class Room:
+
+    def __init__(self) -> None:
+        self.area: int = 0
+        self.objects: list[Lab_object] = []
+
+
+class Lab_object:
+
+    def __init__(self) -> None:
+        self.obj_type_code: str = ""
+        self.size: int = 0
+        self.weight: int = 0
+        self.max_weight: int = 0
+        self.contains: Lab_object | None = None
+
+    def show_info(self) -> None:
+        print(f"Obj type: {self.obj_type_code}, contains: {self.contains}")
+
+
+class Worker:
+
+    def __init__(self) -> None:
+        self.exists: bool = False
+        
+
